0c0e133f9ee7d73546e8500f0119f1aff497adbf
Jacob LeCoq <lecoqjacob@gmail.com>
2025-02-25 14:19:04 -0600

    setup dots

---
+++ .install-password-manager.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# exit immediately if password-manager-binary is already in $PATH
+type bw >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+  brew install bitwarden-cli
+  ;;
+Linux)
+  brew install bitwarden-cli
+  ;;
+*)
+  echo "unsupported OS"
+  exit 1
+  ;;
+esac
